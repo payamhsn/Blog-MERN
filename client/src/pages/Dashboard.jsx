f5c5808f20f7a10021ff012085b48ca60fd736f5
Payam <p.hoseini.ch@gmail.com>
2024-01-13 18:02:13 +0330

    Make dashboard front by dividing dashboard to 2 components.First is dashboard sidebar and second in dashboard profile.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  //
+  //Get URL address from browser:
+
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  //
+  //Each time location changes,this code runs and stores tab section from URL :
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* Sidebar Section */}
+        <DashSidebar />
+      </div>
+      {/* Profile Section */}
+
+      {/* When tab in URL change to profile, render DashProfile : */}
+
+      {tab === "profile" && <DashProfile />}
+    </div>
+  );
 }
