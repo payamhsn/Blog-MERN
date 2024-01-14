11dc91c5d00ee6489ca282ab4b7055b47dc687b5
Payam <p.hoseini.ch@gmail.com>
2024-01-14 18:51:36 +0330

    Make SignOut buttons functional. POST req will send to server, and server response with clearing cookie (access_token) from browser.Also change global states with Redux.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,6 +11,7 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSignOutAlt, FaSun } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleTheme } from "../redux/theme/themeSlice";
+import { signoutSuccess } from "../redux/user/userSlice";
 
 export default function Header() {
   const path = useLocation().pathname;
@@ -21,6 +22,22 @@ export default function Header() {
 
   const { currentUser } = useSelector((state) => state.user);
   const { theme } = useSelector((state) => state.theme);
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <Navbar className="border-b-2">
       <Datepicker className="hidden lg:inline w-40 font-san-serif " />
@@ -72,7 +89,9 @@ export default function Header() {
               <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
             <Dropdown.Divider />
-            <Dropdown.Item icon={FaSignOutAlt}>Sign out</Dropdown.Item>
+            <Dropdown.Item icon={FaSignOutAlt} onClick={handleSignout}>
+              Sign out
+            </Dropdown.Item>
           </Dropdown>
         ) : (
           <Link to="/sign-in">
