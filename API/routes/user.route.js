5b4102773243243f4d2cf1decc04277705a61843
Payam <p.hoseini.ch@gmail.com>
2024-01-16 22:22:35 +0330

    Show the comments of a post.

--- API/routes/user.route.js
+++ API/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {
+  deleteUser,
+  getUser,
+  getUsers,
+  signout,
   test,
   updateUser,
-  getUsers,
-  deleteUser,
-  signout,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -15,5 +16,6 @@ router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
 router.get("/getusers", verifyToken, getUsers);
+router.get("/:userId", getUser);
 
 export default router;
